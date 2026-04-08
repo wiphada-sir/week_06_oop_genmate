48cc78a99e1b0567fcbc2f595dd0d1133d22841e
spolmag <spolmag@gmail.com>
2026-04-08 14:09:10 +0700

    1st push

---
+++ refacted_oop_drink/Drink.js
@@ -0,0 +1,10 @@
+class Drink {
+  constructor(name, type, onHand) {
+    this.name = name;
+    this.type = type;
+    this.onHand = onHand;
+  }
+}
+
+//Add sub class
+//Mzx ->Beer->defree, Give->Softdrink->sugar, Palm->Juice->madeFrom, O'->Tea->fromCountry, Weaw->Coffee->beanType
